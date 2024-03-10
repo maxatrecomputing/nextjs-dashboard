4697181f1fe09fb2c3114a835503b95ba4583ebf
maxatrecomputing <max@recomputing.us>
2024-03-09 21:41:24 -0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,9 @@ export default function Page() {
           {/* <div className = "h-0 w-0 border-b-[30px] border-1-[20px] border-r-[20px] border-b-black border-1-transparent border-r-transparent"></div> */}
 
           <div className = {styles.shape} />
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          {/* // ... */}
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
